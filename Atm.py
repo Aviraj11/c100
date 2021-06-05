ca9996cec6d35fe17ea0b3119cbb5518a487c830
Aviraj11 <66950025+Aviraj11@users.noreply.github.com>
2021-06-06 00:03:53 +0530

    Update Atm.py

--- Atm.py
+++ Atm.py
@@ -4,20 +4,14 @@ class Atm():
         self.pin_number = pin_number
 
     def cashWithDrawl(self):
-        card = input("Enter your Card Number here.")
-        pin = input("Enter your Pin Number here.")
         cash = input("Enter The Amount of Cash you Want to Withdrawl.")
         print("Cash Withdarwled Successfully.")
     
     def cashDeposit(self):
-        card = input("Enter your Card Number here.")
-        pin = input("Enter your Pin Number here.")
         cash = input("Enter The Amount of Cash you Want to Deposit.")
         print("Cash Deposited Successfully.")
     
     def balanceEnquiry(self):
-        card = input("Enter your Card Number here.")
-        pin = input("Enter your Pin Number here.")
         print("Balance Enquired Successfully.")
 
 Aviraj = Atm(123,456)
